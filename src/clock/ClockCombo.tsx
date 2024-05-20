555f690c11dc5f869ce0c06398366ba1d990892b
yyang2018mel <michael.yangyifan@gmail.com>
2024-05-21 09:45:14 +1000

    Allow change background image

--- src/clock/ClockCombo.tsx
+++ src/clock/ClockCombo.tsx
@@ -5,17 +5,22 @@ import AnalogClock from "./AnalogClock";
 import DigitalClock from "./DigtalClock";
 import { ClockImageUrls } from "./BackgroundImages";
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   BottomNavigation,
   BottomNavigationAction,
   Button,
   Dialog,
   DialogActions,
+  DialogContent,
   DialogTitle,
 } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import ScienceIcon from "@mui/icons-material/Science";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import PictureSelectionGrid from "../generic/PictureSelector";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function ClockCombo(prop: { clockSize: number }): React.JSX.Element {
   const { clockSize } = prop;
@@ -28,24 +33,61 @@ function ClockCombo(prop: { clockSize: number }): React.JSX.Element {
   const [clockUserMode, setClockUserMode] = useState<ClockUserMode>("Static");
   const prevClockUserMode = useRef<ClockUserMode>(clockUserMode);
 
+  const [backgroundImageUrl, setBackgroundImageUrl] = useState<string>("");
+  const backgroundImageUrlRef = useRef<string>(backgroundImageUrl);
+
   return (
     <ClockContext.Provider value={{ clockState, setClockState }}>
-      <AnalogClock clockSize={clockSize} backgroundImageUrl="" />
+      <AnalogClock
+        clockSize={clockSize}
+        backgroundImageUrl={backgroundImageUrl}
+      />
       <div style={{ height: 10 }} />
       <DigitalClock />
       <div style={{ height: 10 }} />
 
       <Dialog
         open={clockUserMode === "Setup"}
+        scroll="paper"
         onClose={() => setClockUserMode(prevClockUserMode.current)}
       >
-        <DialogTitle fontSize={12}>Configuration</DialogTitle>
-        {/* <PictureSelectionGrid
-          selectionType="single"
-          candidateImages={ClockImageUrls}
-        /> */}
+        <DialogTitle>Configuration</DialogTitle>
+        <DialogContent>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              Background Image
+            </AccordionSummary>
+            <AccordionDetails>
+              <PictureSelectionGrid
+                candidateImages={ClockImageUrls}
+                onPictureSelection={(url) =>
+                  (backgroundImageUrlRef.current = url)
+                }
+              />
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              Clock Hand
+            </AccordionSummary>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              Text
+            </AccordionSummary>
+          </Accordion>
+        </DialogContent>
+
         <DialogActions>
-          <Button size="small">Apply</Button>
+          <Button
+            size="small"
+            onClick={() => {
+              setBackgroundImageUrl(backgroundImageUrlRef.current);
+              setClockUserMode(prevClockUserMode.current);
+            }}
+          >
+            Apply
+          </Button>
         </DialogActions>
       </Dialog>
 
--- src/generic/PictureSelector.tsx
+++ src/generic/PictureSelector.tsx
@@ -3,34 +3,35 @@ import React from "react";
 
 function PictureSelectionGrid({
   candidateImages,
-  selectionType,
+  onPictureSelection,
 }: {
-  selectionType: "single" | "multiple";
   candidateImages: string[];
+  onPictureSelection: (url: string) => void;
 }) {
-  const [selected, setSelected] = React.useState<Record<number, boolean>>({});
+  const [selected, setSelected] = React.useState<number | null>(null);
 
   const handleSelect = (index: number) => {
-    setSelected((prevSelected) => ({
-      ...prevSelected,
-      [index]: !prevSelected[index],
-    }));
+    setSelected(index === selected ? null : index);
+    onPictureSelection(candidateImages[index]);
   };
 
   return (
     <Grid container spacing={3}>
       {candidateImages.map((url, index) => (
         <Grid item xs={4} key={index}>
-          <Card onClick={() => handleSelect(index)}>
+          <Card
+            onClick={() => handleSelect(index)}
+            style={
+              selected === index
+                ? { borderStyle: "solid", borderWidth: 2, borderColor: "blue" }
+                : {}
+            }
+          >
             <CardMedia
               component="img"
               alt={`Image ${index}`}
               height="140"
               image={url}
-            />
-            <Checkbox
-              checked={!!selected[index]}
-              onChange={() => handleSelect(index)}
             />
           </Card>
         </Grid>
